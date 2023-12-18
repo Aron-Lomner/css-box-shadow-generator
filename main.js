70cc460d22c4d05bdf30ec417a604b12307b4b49
unstack-underflow <aronlomn@gmail.com>
2023-12-17 22:17:59 -0500

    Changed #box background-color}| styled sliders for firefox and chrome

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import {funcGetRGBA} from "/css-box-shadow-generator/colorConverter.js";
+import { funcGetRGBA } from "/colorConverter.js";
 
 const box = document.querySelector("#box");
 const sldrOffsetX = document.querySelector("#sldrOffsetX");
@@ -10,7 +10,7 @@ const colorPicker = document.querySelector("#shadowcolor");
 const hOffsetValue = document.querySelector("#hOffsetValue");
 const vOffsetValue = document.querySelector("#vOffsetValue");
 const blurRadiusValue = document.querySelector("#blurRadiusValue");
-const spreadRadiusValue= document.querySelector("#spreadRadiusValue");
+const spreadRadiusValue = document.querySelector("#spreadRadiusValue");
 const opacityValue = document.querySelector("#opacityValue");
 
 const cssCode = document.querySelector("#css-code");
@@ -18,31 +18,29 @@ const cssCode = document.querySelector("#css-code");
 let boxShadowValue = "0px 0px 0px 0px rgba(0,0,0,1)";
 
 const updateBoxShadow = () => {
-    
-    let v1 = sldrOffsetX.value;    
-    hOffsetValue.innerHTML = v1;
-    let v2 = sldrOffsety.value;
-    vOffsetValue.innerHTML = v2;
-    let v3 = sldrBlurRadius.value;
-    blurRadiusValue.innerHTML = v3;
-    let v4 = sldrSpreadRadius.value;
-    spreadRadiusValue.innerHTML = v4;
-    let v5 = sldrOpacity.value/100;   
-    opacityValue.innerHTML = v5;
-    let color = funcGetRGBA(colorPicker.value);
+  let v1 = sldrOffsetX.value;
+  hOffsetValue.innerHTML = v1;
+  let v2 = sldrOffsety.value;
+  vOffsetValue.innerHTML = v2;
+  let v3 = sldrBlurRadius.value;
+  blurRadiusValue.innerHTML = v3;
+  let v4 = sldrSpreadRadius.value;
+  spreadRadiusValue.innerHTML = v4;
+  let v5 = sldrOpacity.value / 100;
+  opacityValue.innerHTML = v5;
+  let color = funcGetRGBA(colorPicker.value);
 
-    let regex = /,[.0-9]+\)/g;
-    
-    boxShadowValue = `${v1}px ${v2}px ${v3}px ${v4}px ${color}`;
-    boxShadowValue = boxShadowValue.replace(regex, `,${v5})`);
-    
-    cssCode.value = `box-shadow: ${boxShadowValue};`;
-    
-    box.style.boxShadow = boxShadowValue;
-}
+  let regex = /,[.0-9]+\)/g;
+
+  boxShadowValue = `${v1}px ${v2}px ${v3}px ${v4}px ${color}`;
+  boxShadowValue = boxShadowValue.replace(regex, `,${v5})`);
+
+  cssCode.value = `box-shadow: ${boxShadowValue};`;
+
+  box.style.boxShadow = boxShadowValue;
+};
 
 updateBoxShadow();
-
 
 sldrOffsetX.addEventListener("input", updateBoxShadow);
 sldrOffsety.addEventListener("input", updateBoxShadow);
@@ -52,12 +50,14 @@ sldrOpacity.addEventListener("input", updateBoxShadow);
 colorPicker.addEventListener("input", updateBoxShadow);
 
 function copyToClipboard() {
-    var inputElement = document.getElementById("css-code");
-    inputElement.select();
-    document.execCommand("copy");
-    window.getSelection().removeAllRanges();
-    let copied = document.getElementById("copied");
-    copied.style.opacity = 1;
-    setTimeout(()=>{copied.style.opacity = 0},350);
+  var inputElement = document.getElementById("css-code");
+  inputElement.select();
+  document.execCommand("copy");
+  window.getSelection().removeAllRanges();
+  let copied = document.getElementById("copied");
+  copied.style.opacity = 1;
+  setTimeout(() => {
+    copied.style.opacity = 0;
+  }, 350);
 }
 document.getElementById("copy").onclick = copyToClipboard;
